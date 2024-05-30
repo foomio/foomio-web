fe5f9409277321a4139dca2a7f4fc96e1a2ed6a0
foomi <134332353+foomio@users.noreply.github.com>
2024-05-30 09:57:51 -0500

    added new project to project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,29 +5,38 @@ import './styles/Projects.css'; // Import the CSS file
 import Project1Image from './images/1584175590822 (1).png';
 import Project2Image from './images/pink-notes.png'; // Add the new project image
 import Project3Image from './images/logo7.png';
+import Project4Image from './images/Screenshot 2024-05-30 094626.png';
 function Projects() {
   // Sample project data
   const projects = [
+    
     {
       id: 1,
+      title: 'Minikube Project',
+      coverArt: Project4Image, // Use the imported image
+      uploadDate: 'May 30, 2024', // Upload date
+      url: 'https://github.com/foomio/k8s-proj' // External link URL
+    },
+    {
+      id: 2,
       title: 'Tic Tac Toe on React',
       coverArt: Project1Image, // Use the imported image
       uploadDate: 'May 17, 2024', // Upload date
       url: 'https://github.com/foomio/tic-tac-toe-on-react' // External link URL
     },
     {
-      id: 2,
+      id: 3,
       title: 'Notes App with TS',
       coverArt: Project2Image, // Use the new imported image
       uploadDate: 'May 14, 2024', // Upload date
       url: 'https://github.com/foomio/notes-app' // External link URL
     },
     {
-      id: 3,
+      id: 4,
       title: 'Portfolio Site Code',
       coverArt: Project3Image, // Use the new imported image
       uploadDate: 'May 1, 2024', // Upload date
-      url: 'https://github.com/foomio/notes-app' // External link URL
+      url: 'https://github.com/foomio/foomio-web' // External link URL
     },
   ];
 
